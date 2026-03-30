78a5e6aac96c09fe5f9187272744e5a2c1b43c17
JishDey <jishdey05@gmail.com>
2026-03-30 19:14:24 -0400

    added kurtosis and skew difference measurements, as well as some general progress

--- model_failures.py
+++ model_failures.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis
 
 # load processed dataset (TODO: to be extended to multiple datasets)
 df = pd.read_csv('lifetime_data.csv')
@@ -23,6 +24,8 @@ def estimate_geometric():
 
     mean_T = np.mean(T)
     var_T = np.var(T, ddof=1)
+    skew_T = skew(T)
+    kurt_T = kurtosis(T)
 
     p_hat = 1 / mean_T
 
@@ -30,11 +33,15 @@ def estimate_geometric():
     print(f"Empirical Variance: {var_T:.4f}")
     print(f"Estimated p (MLE): {p_hat:.6f}")
 
-    return p_hat, mean_T, var_T
+    return p_hat, mean_T, var_T, skew_T, kurt_T
 
-def compare_stats(p_hat, empirical_mean, empirical_var):
+def compare_stats(p_hat, empirical_mean, empirical_var, empirical_skew, 
+                                                        empirical_kurt):
     theoretical_mean = 1 / p_hat
     theoretical_var = (1 - p_hat) / (p_hat ** 2)
+    theoretical_skew = (2 - p_hat) / np.sqrt(1 - p_hat)
+    theoretical_kurt = 6 + (p_hat ** 2) / (1 - p_hat)
+
 
     print("\n--- Comparison ---")
     print(f"Theoretical Mean: {theoretical_mean:.4f}")
@@ -42,6 +49,12 @@ def compare_stats(p_hat, empirical_mean, empirical_var):
     print("         - - -")
     print(f"Theoretical Var:  {theoretical_var:.4f}")
     print(f"Empirical Var:    {empirical_var:.4f}")
+    print("         - - -")
+    print(f"Theoretical Skew: {theoretical_skew:.4f}")
+    print(f"Empirical Skew:   {empirical_skew:.4f}")
+    print("         - - -")
+    print(f"Theoretical Kurt: {theoretical_kurt:.4f}")
+    print(f"Empirical Kurt:   {empirical_kurt:.4f}")
 
 def simulate_geometric(p_hat, N=10000):
     simulated = np.random.geometric(p_hat, size=N)
@@ -60,8 +73,8 @@ def plot_comparison(real_data, simulated_data):
 
 def main():
     check_failed()
-    p_hat, mean_T, var_T = estimate_geometric()
-    compare_stats(p_hat, mean_T, var_T)
+    p_hat, mean_T, var_T, skew_T, kurt_T = estimate_geometric()
+    compare_stats(p_hat, mean_T, var_T, skew_T, kurt_T)
     simulated = simulate_geometric(p_hat, N=len(failed_df))
     plot_comparison(failed_df['lifetime_days'], simulated)
 
--- stack_tables.py
+++ stack_tables.py
@@ -5,20 +5,27 @@ import os
 # You can check the data by running the file interactively and calling `check`
 
 # the data directory, containing csv files
+# this block could be improved by os.walk, but this is good enough for now 
 data_dir = 'data'
-list_of_files = os.listdir(data_dir)
+data_subdirs = os.listdir(data_dir)
 dfs = []
-for i, file in enumerate(list_of_files):
-    df = pd.read_csv(f'{data_dir}/{file}')
-    dfs.append(df)
-    print(f"file number {i}")
-    if i > 25:
-        break
+for i, subdir in enumerate(data_subdirs):
+    for j, file in enumerate(os.listdir(data_dir + "/" + subdir)):
+        df = pd.read_csv(f'{data_dir}/{subdir}/{file}', 
+                         usecols=['date', 'serial_number', 'model', 'failure'])
+        df['date'] = pd.to_datetime(df['date'])
+        df['serial_number'] = df['serial_number'].astype('category')
+        df['model'] = df['model'].astype('category')
+        dfs.append(df)
+        print(f"folder number {i}, file number {j}")
 
 # stacking the dataframes into one - like an 'append'
 stacked_df = pd.concat(dfs)
-stacked_df = stacked_df[['date', 'serial_number', 'model', 'failure']]
-stacked_df['date'] = pd.to_datetime(stacked_df['date'])
+
+# make sure these aren't needed
+# stacked_df = stacked_df[['date', 'serial_number', 'model', 'failure']] 
+# stacked_df['date'] = pd.to_datetime(stacked_df['date'])
+
 stacked_df = stacked_df.sort_values(['serial_number', 'date']).reset_index(drop=True)
 
 # grouping the data so that each drive (marked by serial) is one
